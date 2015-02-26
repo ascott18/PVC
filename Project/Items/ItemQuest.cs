8c43a3faf3292360875a6d81600578bb68a0adf6
Elise Peterson <e.peterson2014@hotmail.com>
2015-02-26 10:31:41 -0800

    added itemquest class

---
+++ Project/Items/ItemQuest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Project.Data;
+using Project.Sprites;
+
+namespace Project.Items
+{
+    internal class ItemQuest : Item
+    {
+        public ItemQuest(int itemId, string name) : base(itemId, name)
+		{
+
+		}
+
+        [XmlData.XmlParserAttribute("Quest")]
+        public static Item ParseItem(XElement itemElement)
+        {
+            var id = int.Parse(itemElement.Attribute("id").Value);
+            var name = itemElement.Attribute("name").Value;
+            return new ItemQuest(id, name);
+        }
+
+        public override string GetTooltip()
+        {
+            var s = base.GetTooltip();
+            s += "\nQuest Item";
+
+            return s;
+        }
+    }
+}
