1806e550d6f1a05e148b48f692b5f29a12e0f4ba
Andrew Scott <ascott18@gmail.com>
2015-03-09 20:44:30 -0700

    Added an attribute to prevent looted chests from despawning

--- Project/Dungeon/Chest.cs
+++ Project/Dungeon/Chest.cs
@@ -12,7 +12,10 @@ namespace Project.Dungeon
 {
     class Chest : TileObject
     {
-        private List<LootPool> lootPools; 
+        private List<LootPool> lootPools;
+		public bool ShouldDespawn { get; private set; }
+		public bool Looted { get; private set; }
+
 
         protected Chest(Point loc) : base(loc)
         {
@@ -20,6 +23,9 @@ namespace Project.Dungeon
 
         public override void Interact(Game game)
         {
+	        if (Looted) return;
+	        Looted = true;
+
             var loot = new List<Item>();
             foreach (var lootPool in lootPools)
             {
@@ -31,7 +37,9 @@ namespace Project.Dungeon
             dialog.ShowDialog();
 
             game.Party.AddInventoryItemRange(loot);
-            CurrentTile.TileObject = null;
+
+			if (ShouldDespawn)
+			   CurrentTile.TileObject = null;
         }
 
 
@@ -45,10 +53,13 @@ namespace Project.Dungeon
 		{
 			var loc = new Point(int.Parse(chestElement.Attribute("x").Value), int.Parse(chestElement.Attribute("y").Value));
 
+			var despawnElement = chestElement.Attribute("despawn");
+
             var chest = new Chest(loc)
             {
 	            Image = XmlData.LoadImage(chestElement.Attribute("texture").Value),
-	            lootPools = LootPool.ParseLootPools(chestElement)
+	            lootPools = LootPool.ParseLootPools(chestElement),
+				ShouldDespawn = despawnElement == null || (bool)despawnElement
             };
 
 			return chest;
